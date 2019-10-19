112a93aa73cba68b7e40d55d3e3837db0715a2de
yt0379 <yt0379@qq.com>
2019-10-19 22:54:12 +0800

    fff

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,68 @@
-const pkg = require('./package')
-
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+   ** Global CSS
+   */
+  css: ["element-ui/lib/theme-chalk/index.css"],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      // routes已经生成的路由
+      // resolve是获取页面方法
+      routes.push({
+        name: "foo",
+        path: "/foo",
+        component: resolve(__dirname, "pages/custom.vue"),
+      });
+    },
+  },
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["@/plugins/element-ui"],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios", "cookie-universal-nuxt"],
+  axios: {
+    proxy: true,
+  },
+  proxy: {
+    "/api": "http://localhost:8080",
+  },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     transpile: [/^element-ui/],
-    
+
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
-}
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,42 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-test
-      </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h2>商品列表</h2>
+    <ul>
+      <li v-for="good in goods" :key="good.id">
+        <nuxt-link :to="`/detail/${good.id}`">
+          <span>{{good.text}}</span>
+          <span>￥{{good.price}}</span>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  head() {
+    return {
+      title: "课程列表",
+      meta: [
+        { name: "description", hid: "description", content: "set page meta" }
+      ],
+      link: [{ rel: "favicon", href: "favicon.ico" }]
+    };
+  },
+  async asyncData({ $axios, error }) {
+    // 页面上下文
+    const { ok, goods } = await $axios.$get("/api/goods");
+    if (ok) {
+      return { goods };
+    }
+    // 错误处理
+    error({ statusCode: 400, message: "数据查询失败" });
   }
-}
+  // data() {
+  //   return { goods: [
+  //     {id:1, text:'Web全栈架构师',price:8999},
+  //     {id:2, text:'Python全栈架构师',price:8999},
+  //   ] }
+  // }
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
