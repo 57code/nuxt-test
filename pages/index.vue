168cf08e9565629872e882827a68aa1871055ad4
yt0379 <yt0379@qq.com>
2019-11-13 17:38:11 +0800

    预先准备api服务

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,6 @@
       <h1 class="title">
         nuxt-test
       </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
     </div>
   </section>
 </template>
