2ca30c45b8181d257382a4316771da12ba35a7af
yt0379 <yt0379@qq.com>
2019-11-13 22:41:38 +0800

    nuxt初体验

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,24 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-test
-      </h1>
-    </div>
-  </section>
+  <div>
+    <h2>商品列表</h2>
+    <ul>
+      <li v-for="good in goods" :key="good.id" >
+        <nuxt-link :to="`/detail/${good.id}`">
+          <span>{{good.text}}</span>
+          <span>￥{{good.price}}</span>
+        </nuxt-link>        
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return { goods: [
+      {id:1, text:'Web全栈架构师',price:8999},
+      {id:2, text:'Python全栈架构师',price:8999},
+    ] }
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        login page
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'blank'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
